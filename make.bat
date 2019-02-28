d7e39eb1fadfb3cb19d66b246b9f86819a4b3eaa
Lauris Vāvere <lauris@ma-1.lv>
2019-02-28 15:02:33 +0200

    fix bugs

--- make.bat
+++ make.bat
@@ -8,7 +8,8 @@ set BUILD=1
 cd repo
 
 echo ... wix heat
-heat dir node_modules -nologo -ag -cg modules -dr INSTALLLOCATION -var env.NodeModules -o %MODULES%.wxs
+set NodeModules=./node_modules
+heat dir ./node_modules -nologo -ag -cg modules -dr INSTALLLOCATION -var env.NodeModules -o %MODULES%.wxs
 
 echo ... wix candle
 candle  -nologo -dbuild="%BUILD%" -arch x86 %PROJECT%.wxs %MODULES%.wxs
