259f6576cf0f5fa90e795a4d3bc12336134aa16b
Lauris Vāvere <lauris@ma-1.lv>
2019-02-28 15:00:27 +0200

    fix typos #2

--- make.bat
+++ make.bat
@@ -8,12 +8,12 @@ set BUILD=1
 cd repo
 
 echo ... wix heat
-heat dir node_modules -nologo -ag -cg modules -dr INSTALLLOCATION -var env.NodeModules -o %MODULES%.wx
+heat dir node_modules -nologo -ag -cg modules -dr INSTALLLOCATION -var env.NodeModules -o %MODULES%.wxs
 
 echo ... wix candle
 candle  -nologo -dbuild="%BUILD%" -arch x86 %PROJECT%.wxs %MODULES%.wxs
 
 echo ... wix light
-light -nologo -b ..\ -cultures:lv-LV -dWixUILicenseRtf=licence.rtf -sice:ICE03 -sw1105 -sw1076 -spdb -ext WixUIExtension %PROJECT%.wixobj %MODULES%.wixobj -o %PROJECT%.msi
+light -nologo -b .\ -cultures:lv-LV -dWixUILicenseRtf=licence.rtf -sice:ICE03 -sw1105 -sw1076 -spdb -ext WixUIExtension %PROJECT%.wixobj %MODULES%.wixobj -o %PROJECT%.msi
 
 
