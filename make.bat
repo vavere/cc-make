beee53c8295e84e93f603478c46574ed94ade9e7
Lauris Vāvere <lauris@ma-1.lv>
2019-03-01 20:38:00 +0200

    new make version

--- make.bat
+++ make.bat
@@ -1,19 +1,11 @@
-@ECHO off
 set PATH=%PATH%%WIX%bin
 
-echo ... wix heat
 set NodeModules=./node_modules
 heat dir ./node_modules -v -nologo -ag -cg modules -dr INSTALLLOCATION -var env.NodeModules -o modules.wxs
 
-echo ... wix candle
 candle -v -nologo -dbuild="%BUILD%" -dPID="%PID%" -arch x86 %PNAME%.wxs modules.wxs
 
-echo ... wix light
-light -v -nologo -b .\ -cultures:lv-LV -sice:ICE03 -sw1105 -sw1076 -spdb -ext WixUIExtension %PNAME%.wixobj modules.wixobj -o %PNAME%.msi
+light -v -nologo -b .\ -cultures:lv-LV -spdb -ext WixUIExtension %PNAME%.wixobj modules.wixobj -o %OUTPUT%\%PNAME%.msi
 
-echo ... copy to server	
-copy %PNAME%.msi %DEPLOY% /y	
-
-echo ... create repair	
-echo msiexec /x {%PID%} /qb >%DEPLOY%\%PNAME%_repair.cmd	
-echo msiexec /i %%~dp0\%PNAME%.msi /l*vx %%~dp0\%PNAME%.log /qb >>%DEPLOY%\%PNAME%_repair.cmd
+echo msiexec /x {%PID%} /qb >%OUTPUT%\%PNAME%_repair.cmd	
+echo msiexec /i %%~dp0\%PNAME%.msi /l*vx %%~dp0\%PNAME%.log /qb >>%OUTPUT%\%PNAME%_repair.cmd
