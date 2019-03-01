3009743e47f39716fdaa6e636c251ae9b0ee69dc
Lauris Vāvere <lauris@ma-1.lv>
2019-03-01 20:41:24 +0200

    better with echo

--- make.bat
+++ make.bat
@@ -1,11 +1,16 @@
+@echo off
 set PATH=%PATH%%WIX%bin
 
+echo heat ...
 set NodeModules=./node_modules
-heat dir ./node_modules -v -nologo -ag -cg modules -dr INSTALLLOCATION -var env.NodeModules -o modules.wxs
+heat dir ./node_modules -nologo -ag -cg modules -dr INSTALLLOCATION -var env.NodeModules -o modules.wxs
 
-candle -v -nologo -dbuild="%BUILD%" -dPID="%PID%" -arch x86 %PNAME%.wxs modules.wxs
+echo candle ...
+candle -nologo -dbuild="%BUILD%" -dPID="%PID%" -arch x86 %PNAME%.wxs modules.wxs
 
-light -v -nologo -b .\ -cultures:lv-LV -spdb -ext WixUIExtension %PNAME%.wixobj modules.wixobj -o %OUTPUT%\%PNAME%.msi
+echo light ...
+light -nologo -b .\ -cultures:lv-LV -spdb -ext WixUIExtension %PNAME%.wixobj modules.wixobj -o %OUTPUT%\%PNAME%.msi
 
+echo repair.cmd ...
 echo msiexec /x {%PID%} /qb >%OUTPUT%\%PNAME%_repair.cmd	
 echo msiexec /i %%~dp0\%PNAME%.msi /l*vx %%~dp0\%PNAME%.log /qb >>%OUTPUT%\%PNAME%_repair.cmd
