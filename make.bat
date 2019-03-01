08d58b16be96890f1a3457dd900beadb3b7fbe4c
Lauris Vāvere <lauris@ma-1.lv>
2019-03-01 14:05:33 +0200

    add verbose info

--- make.bat
+++ make.bat
@@ -3,13 +3,13 @@ set PATH=%PATH%%WIX%bin
 
 echo ... wix heat
 set NodeModules=./node_modules
-heat dir ./node_modules -nologo -ag -cg modules -dr INSTALLLOCATION -var env.NodeModules -o modules.wxs
+heat dir ./node_modules -v -nologo -ag -cg modules -dr INSTALLLOCATION -var env.NodeModules -o modules.wxs
 
 echo ... wix candle
-candle -nologo -dbuild="%BUILD%" -dPID="%PID%" -arch x86 %PNAME%.wxs modules.wxs
+candle -v -nologo -dbuild="%BUILD%" -dPID="%PID%" -arch x86 %PNAME%.wxs modules.wxs
 
 echo ... wix light
-light -nologo -b .\ -cultures:lv-LV -sice:ICE03 -sw1105 -sw1076 -spdb -ext WixUIExtension %PNAME%.wixobj modules.wixobj -o %PNAME%.msi
+light -v -nologo -b .\ -cultures:lv-LV -sice:ICE03 -sw1105 -sw1076 -spdb -ext WixUIExtension %PNAME%.wixobj modules.wixobj -o %PNAME%.msi
 
 echo ... copy to server	
 copy %PNAME%.msi %DEPLOY% /y	
